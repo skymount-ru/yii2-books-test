b63bb4363ecfd3d3451ccd07d549802f8e8d4760
Dmitry Vikharev <idexdv@gmail.com>
2020-02-06 13:29:04 +0300

    Created data models: Author, Book

---
+++ models/Book.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+class Book extends db\Book
+{
+    
+}
---
+++ models/db/Author.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace app\models\db;
+
+use Yii;
+
+/**
+ * This is the model class for table "authors".
+ *
+ * @property int $id
+ * @property string $name
+ *
+ * @property Books[] $books
+ */
+class Author extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'authors';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['name'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+        ];
+    }
+
+    /**
+     * Gets query for [[Books]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getBooks()
+    {
+        return $this->hasMany(Books::className(), ['author_id' => 'id']);
+    }
+}
